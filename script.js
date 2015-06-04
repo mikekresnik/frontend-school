40d1ebaf8d09f166c3d53517283f322f3734f736
Mike Kresnik <mike@cdomaha.com>
2015-06-03 20:57:28 -0500

    moved CSS and JS to individual files

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("myLink").style.fontWeight = "bold";
+console.log("I'm logging things!");
+
+document.getElementById("mySpan").onclick = function() {
+alert('This is a test');
+};
